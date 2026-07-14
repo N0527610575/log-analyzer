41eb8db85159f907ad8546d337b32870e4314bac
nahman <n0527610575@gmail.com>
2026-07-14 11:16:46 +0300

    הוספת קובץ main

--- checks.py
+++ checks.py
@@ -1,9 +1,12 @@
 from reader import reader_log
 
-def check_ip(file_ips,):
+def safe_port(log_file):
     sensitiv_ips = ["23","22","3389"]
 
-    safe_row = filter(lambda row: row[3] not in sensitiv_ips, file_ips)
-    sensitiv_row =  [row for row in file_ips if row[3] not in sensitiv_ips]
+    safe_row = filter(lambda row: row[3] not in sensitiv_ips, log_file)
 
     return list(safe_row)
+
+def tag_sizes(log_file):
+    size_tags = map(lambda row: ("NORMAL" if int(row[5]) <= 5000 else "LARGE", row), log_file)
+    return list(size_tags)
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-# main.py
-from reader import reader_log
-from checks import  check_ip
-def main():
-    log_data = reader_log("network_traffic.log")
 
-    for row in log_data[:9]:
-        print(row)
+
+
+from reader import reader_log
+from checks import safe_port
+from checks import tag_sizes
+
+
+
+def main():
+    log_file = "network_traffic.log"
+    log_data = reader_log(log_file)
+    print(f"מספר השורות{len(log_file)}")
+
+    print(f"מספר השורות עם פורטים תקינים{len(safe_port(log_data))}")
+
+    tag_row = tag_sizes(log_data)
+
+    tag_name = [(tag, row) for tag , row in tag_row]
+    print(tag_name[:5])
+
 
 if __name__ == "__main__":
     main()
